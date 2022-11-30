ab8031cc29042c6f0bdcd184f80c11313898525c
Alexiskey <56357176+Alexiskey@users.noreply.github.com>
2022-11-30 11:02:10 -0300

    Update server.py

--- server.py
+++ server.py
@@ -10,123 +10,100 @@ import select
 taskQueue = queue.Queue()
 stopFlag = False
 
-#Para poder calcular la hora actual
 _SYSTEM_EPOCH = datetime.date(*time.gmtime(0)[0:3])
-#Sistema EPOCH
 _NTP_EPOCH = datetime.date(1900, 1, 1)
-#NTP Epoch
 NTP_DELTA = (_SYSTEM_EPOCH - _NTP_EPOCH).days * 24 * 3600
-#Data entre sistema y tiempo NTP
+
 
 def system_to_ntp_time(timestamp):
-    #Convierte un tiempo de sistema a un tiempo NTP, parametros: Timestamp
-    #Retorna: Correspondiente tiempo NTP
+# Combierte la hora a sistema NTP 
+# y devuelve el tiempo correspondiente
     return timestamp + NTP_DELTA
 
 def _to_int(timestamp):
-    #Retorna la parte entera de un Timestamp.
-    #Parametros: Timestamp en formato NTP
-    #Retorna Parte entera
+# se le entrega el tiempo en formato NTP y devuelve la parte entera
     return int(timestamp)
 
 def _to_frac(timestamp, n=32):
-    #Retorna la parte fraccionaria de un Timestamp.
-    #Parametros: Timestamp en formato NTP, n: numero de bits de la parte fraccionaria.
-    #Retorna: Parte fraccionaria
+# se le entrega el tiempo en formato NTP y devuelve la parte fraccional
     return int(abs(timestamp - _to_int(timestamp)) * 2**n)
 
 def _to_time(integ, frac, n=32):
-    #Retorna un Timestamp de la parte integral y fraccionaria.
-    #Parametros: Parte ingregral, parte fraccionaria, n: numero de bits de la parte fraccionaria.
+# toma la parte entera y la parte fraccional 
+# y devuelve la suma
     return integ + float(frac)/2**n	
-		
-
 
 class NTPException(Exception):
-    #Excepcion levantada por este modulo.
     pass
 
-
+# Empaquetado y desempaquetado NTP    
 class NTPPacket:
-    #Paquete de la clase NTP
-    #Esto representa un paquete NTP
-    _PACKET_FORMAT = "!B B B b 11I" # 32 bits I(Entero sin signo), 11I = I(32) * 11 = 352
-    #Formato del paquete para hacer pack y unpack
+    _PACKET_FORMAT = "!B B B b 11I" # B = 1 byte = 8 bits, b = 1 byte = 8 bits 
+    #                                 I = 4 bytes = 32 bits --> 11I = 352 bits 
+    #                |||||||||||||
+    #                vvvvvvvvvvvvv
+    #           8 + 8 + 8 + 8 + 352 = 284 bits
+    #Formato del paquete para hacer empaquetar y desempaquetar
 
     def __init__(self, version=2, mode=3, tx_timestamp=0):
-        #Constructor
-        #Parametros: version (NTP), mode, tx_timestamp(paquete de transmision timestamp)
-        self.leap = 0
-        #Segundo indicador del leap
-        self.version = version
-        #Version
-        self.mode = mode
-        #Modo
-        self.stratum = 0
-        #Stratum
-        self.poll = 0
-        #Invervalo del poll
-        self.precision = 0
-        #Precision
-        self.root_delay = 0
-        #Retraso de la raiz
-        self.root_dispersion = 0
-        #Dispersion de la raiz
-        self.ref_id = 0
-        #Identificador de referencia para el reloj
-        self.ref_timestamp = 0
-        #Timestamp de referencia
-        self.orig_timestamp = 0
-        self.orig_timestamp_high = 0
-        self.orig_timestamp_low = 0
-        #Originar timestamp
-        self.recv_timestamp = 0
-        #Recibe timestamp
-        self.tx_timestamp = tx_timestamp
-        self.tx_timestamp_high = 0
-        self.tx_timestamp_low = 0
-       
+# Constructor NTP, se predefine los valores, dejando
+# leap 0, version 2, modo 3 y los demas todos 0
+        self.leap = 0 # Leap
+        self.version = version # Version
+        self.mode = mode # Modo
+        self.stratum = 0 # Stratum
+        self.poll = 0 # Poll interval
+        self.precision = 0 # Precision
+        self.root_delay = 0 # Root delay
+        self.root_dispersion = 0 # Root dispersion
+
+        self.ref_id = 0 # Reference clock identifier
+        self.ref_timestamp = 0 # Reference Timestamp
         
+        self.orig_timestamp = 0 # Originate Timestamp
+        self.orig_timestamp_high = 0 # Parte entera
+        self.orig_timestamp_low = 0 # Parte fraccionaria
+        
+        self.recv_timestamp = 0 # Receive Timestamp
+
+        self.tx_timestamp = tx_timestamp # Transmit Timestamp:
+        self.tx_timestamp_high = 0 # Parte entera
+        self.tx_timestamp_low = 0 # Parte Fraccionaria
+        
+
     def to_data(self):
-        #Convierte este paquete NTP a un buffer que puede ser enviado a traves de un socket.
-        #Retorna: Buffer representando este paquete.
-        #Contiene una excepcion en caso de un campo invalido
+    # Tansforma el paquete a un archivo que se pueda enviar
+    # y usa el NTPExeption cuando se usa un archivo que no corresponde al formato NTP
         try:
             packed = struct.pack(NTPPacket._PACKET_FORMAT,
                 (self.leap << 6 | self.version << 3 | self.mode),
+                # 00                 010               011   <----- (00010011)
                 self.stratum,
                 self.poll,
                 self.precision,
-#RFC define Timestamp define valores en 64 bits, tiene una parte entera y una parte fraccionaria, dividen los 64 bits en 2, 32 y 32, por eso son 11 eleme    ntos de 32 bits. 
-                #Aca parten los 11 elementos de 32 bits
-
                 _to_int(self.root_delay) << 16 | _to_frac(self.root_delay, 16),
                 _to_int(self.root_dispersion) << 16 |
                 _to_frac(self.root_dispersion, 16),
                 self.ref_id,
                 _to_int(self.ref_timestamp),
                 _to_frac(self.ref_timestamp),
-                #Change by lichen, avoid loss of precision
                 self.orig_timestamp_high,
                 self.orig_timestamp_low,
                 _to_int(self.recv_timestamp),
                 _to_frac(self.recv_timestamp),
                 _to_int(self.tx_timestamp),
                 _to_frac(self.tx_timestamp))
-              #Terminan los 11 elementos de 32 bits
-
         except struct.error:
-            raise NTPException("Invalid NTP packet fields.")
+            raise NTPException("Paquete invalido.")
         return packed
 
     def from_data(self, data):
-        
-	#Rellena esta instancia de una carga util del paquete NTP recibido por la red.
-        #Parametros: Data (buffer con carga util)
-        #Contiene una excepcion en caso que el formato del paquete sea invalido.
+    # Carga el paquete y lo "decodifica" para poder transformar la informacion
+    # a datos con los que se puedan trabajar
+
         try:
             unpacked = struct.unpack(NTPPacket._PACKET_FORMAT,
-                    data[0:struct.calcsize(NTPPacket._PACKET_FORMAT)]) #Arreglo de bytes que deberia recibir
+                    data[0:struct.calcsize(NTPPacket._PACKET_FORMAT)])
         except struct.error:
             raise NTPException("Invalid NTP packet.")
 
@@ -154,8 +131,9 @@ class NTPPacket:
     def SetOriginTimeStamp(self,high,low):
         self.orig_timestamp_high = high
         self.orig_timestamp_low = low
-        
 
+
+# hilo de trabajo para la recepcion del paquete
 class RecvThread(threading.Thread):
     def __init__(self,socket):
         threading.Thread.__init__(self)
@@ -164,57 +142,52 @@ class RecvThread(threading.Thread):
         global taskQueue,stopFlag
         while True:
             if stopFlag == True:
-                print ("RecvThread Ended")
+                print ("RecvThread Terminado")
                 break
             rlist,wlist,elist = select.select([self.socket],[],[],1);
             if len(rlist) != 0:
-                print ("Received %d packets" % len(rlist))
+                print ("Paquete %d Recivido" % len(rlist))
                 for tempSocket in rlist:
                     try:
-                        data,addr = tempSocket.recvfrom(1024) #Recibe el socket, se cuelga el thread hasta que llegue el mensaje
+                        data,addr = tempSocket.recvfrom(1024)
+                        #print (data)
                         recvTimestamp = recvTimestamp = system_to_ntp_time(time.time())
-                        taskQueue.put((data,addr,recvTimestamp)) #Se usa una cola
+                        taskQueue.put((data,addr,recvTimestamp))
                     except socket.error:
                         print ("Error")
 
-class WorkThread(threading.Thread): #Prepara el mensaje de respuesta
+# hilo de trabajo para enviar el paquete devuelta al cliente
+class WorkThread(threading.Thread):
     def __init__(self,socket):
         threading.Thread.__init__(self)
         self.socket = socket
     def run(self):
         global taskQueue,stopFlag
-        while True: #Los threads de trabajo van a estar en un bucle infinito
+        while True:
             if stopFlag == True:
-                print ("WorkThread Ended")
+                print ("WorkThread terminado")
                 break
             try:
-                data,addr,recvTimestamp = taskQueue.get(timeout=1) #Se saca informacion de la cola
-                recvPacket = NTPPacket() #Prepara el paquete de respuesta, el primer elemento de la cola va ser una tupla, de (datos,direccion,hora)
+                data,addr,recvTimestamp = taskQueue.get(timeout=1)
+                recvPacket = NTPPacket()
                 recvPacket.from_data(data)
                 timeStamp_high,timeStamp_low = recvPacket.GetTxTimeStamp()
                 sendPacket = NTPPacket(version=3,mode=4)
                 sendPacket.stratum = 2
-                sendPacket.poll = 10 
-                #Stratum = distancia entre el servidor a un reloj real en una jerarquia de computadores.
-                '''
-                sendPacket.precision = 0xfa
-                sendPacket.root_delay = 0x0bfa
-                sendPacket.root_dispersion = 0x0aa7
-                sendPacket.ref_id = 0x808a8c2c
-                '''
+                sendPacket.poll = 10
                 sendPacket.ref_timestamp = recvTimestamp-5
                 sendPacket.SetOriginTimeStamp(timeStamp_high,timeStamp_low)
                 sendPacket.recv_timestamp = recvTimestamp
                 sendPacket.tx_timestamp = system_to_ntp_time(time.time())
                 socket.sendto(sendPacket.to_data(),addr)
-                print ("Sended to %s:%d" % (addr[0],addr[1]))
+                print ("enviado a %s:%d" % (addr[0],addr[1]))
             except queue.Empty:
                 continue
                 
         
-localIP     = "127.0.0.1" #Se prepara el socket
+localIP     = "192.168.1.108"
 localPort   = 20001
-socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #Se usa protocolo UDP
+socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 socket.bind((localIP,localPort))
 print ("local socket: ", socket.getsockname())
 recvThread = RecvThread(socket)
@@ -226,11 +199,11 @@ while True:
     try:
         time.sleep(0.5)
     except KeyboardInterrupt:
-        print ("Exiting...")
+        print ("Terminando proceso...")
         stopFlag = True
         recvThread.join()
         workThread.join()
         #socket.close()
-        print ("Exited")
+        print ("Terminado")
         break
         
